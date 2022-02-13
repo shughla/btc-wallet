a101b5dd19295ec4096fcc1c8652f6e9c68d1295
Miralllll <55250021+Miralllll@users.noreply.github.com>
2022-02-13 01:16:37 -0500

    One more test

--- app/tests/test_db.py
+++ app/tests/test_db.py
@@ -1,12 +1,15 @@
 import sqlite3
 from sqlite3 import Connection
 
+from app.core.models.transaction import DefaultTransaction
 from app.core.models.user import User
 from app.core.models.wallet import DefaultWallet
-from app.core.repositories import IWalletRepository
+from app.core.repositories import IWalletRepository, ITransactionRepository
 from app.core.security.api_key_generator import ApiKey
+from app.infra.repositories.inmemory.transaction import InMemoryTransactionRepository
 from app.infra.repositories.inmemory.user import InMemoryUserRepository
 from app.infra.repositories.inmemory.wallet import InMemoryWalletRepository
+from app.infra.repositories.sqlite.transaction import SQLiteTransactionRepository
 from app.infra.repositories.sqlite.user import SQLiteUserRepository
 from app.infra.repositories.sqlite.wallet import SQLiteWalletRepository
 
@@ -44,5 +47,21 @@ def test_repository(
     assert len(repo.get_wallets(user)) == 3
 
 
+def test_transaction_with_money(
+        repo: ITransactionRepository = InMemoryTransactionRepository(),
+) -> None:
+    repo_wallet = InMemoryWalletRepository()
+    wallet = DefaultWallet(0)
+    wallet = repo_wallet.add_wallet(wallet)
+    wallet.balance = 2000
+    assert len(repo.find_transaction_by_wallet(wallet.address)) == 0
+    repo.add_transaction(DefaultTransaction(wallet.address, wallet.address, 200, 10))
+    assert len(repo.find_transaction_by_wallet(wallet.address)) == 1
+
+
 def test_sqlite_wallet_repository() -> None:
     test_repository(SQLiteWalletRepository(get_connection()))
+
+
+def test_sqlite_transaction_repository() -> None:
+    test_transaction_with_money(SQLiteTransactionRepository(get_connection()))
